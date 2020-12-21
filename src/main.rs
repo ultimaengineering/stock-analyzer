2c94d2fcf637e5d5b65801be1f5737ad86688ec2
Alexander Montgomery <evaderxander@gmail.com>
2020-12-21 00:02:40 -0500

    update to have just a health route.

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,42 @@ extern crate time;
 use iron::prelude::*;
 use iron::{typemap, AfterMiddleware, BeforeMiddleware};
 use time::{OffsetDateTime};
+use std::{thread};
 
 struct ResponseTime;
+
+struct Health {
+    status: String
+}
 
 impl typemap::Key for ResponseTime { type Value = u64; }
 
 impl BeforeMiddleware for ResponseTime {
     fn before(&self, req: &mut Request) -> IronResult<()> {
-        req.extensions.insert::<ResponseTime>(OffsetDateTime::now_utc()());
+        let t = OffsetDateTime::now_utc() - OffsetDateTime::unix_epoch();
+        req.extensions.insert::<ResponseTime>(t.whole_milliseconds() as u64);
         Ok(())
     }
 }
-
 impl AfterMiddleware for ResponseTime {
     fn after(&self, req: &mut Request, res: Response) -> IronResult<Response> {
-        let delta = OffsetDateTime::now_utc()() - *req.extensions.get::<ResponseTime>().unwrap();
-        println!("Request took: {} ms", (delta as f64) / 1000000.0);
+        let t = OffsetDateTime::now_utc() - OffsetDateTime::unix_epoch();
+        let delta = t.whole_milliseconds() as u64 - *req.extensions.get::<ResponseTime>().unwrap();
+        println!("Request took: {} ms", (delta as f64));
         Ok(res)
     }
 }
 
-fn hello_world(_: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((iron::StatusCode::OK, "Hello World")))
+fn health(_: &mut Request) -> IronResult<Response> {
+    let health = Health {
+        status: String::from("up")
+    };
+    Ok(Response::with((iron::status::Ok, "status: up")))
 }
 
 fn main() {
-    let mut chain = Chain::new(hello_world);
+    let mut chain = Chain::new(health);
     chain.link_before(ResponseTime);
     chain.link_after(ResponseTime);
-    Iron::new(chain).http("localhost:3000");
+    Iron::new(chain).http("localhost:3000").unwrap();
 }
