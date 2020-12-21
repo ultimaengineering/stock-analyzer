667593c5054a28d1b6e004ece7a43de61c5c2743
Alexander Montgomery <evaderxander@gmail.com>
2020-12-20 23:10:20 -0500

    Updates requests with webservers as a starting point.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+extern crate iron;
+extern crate time;
+
+use iron::prelude::*;
+use iron::{typemap, AfterMiddleware, BeforeMiddleware};
+use time::{OffsetDateTime};
+
+struct ResponseTime;
+
+impl typemap::Key for ResponseTime { type Value = u64; }
+
+impl BeforeMiddleware for ResponseTime {
+    fn before(&self, req: &mut Request) -> IronResult<()> {
+        req.extensions.insert::<ResponseTime>(OffsetDateTime::now_utc()());
+        Ok(())
+    }
+}
+
+impl AfterMiddleware for ResponseTime {
+    fn after(&self, req: &mut Request, res: Response) -> IronResult<Response> {
+        let delta = OffsetDateTime::now_utc()() - *req.extensions.get::<ResponseTime>().unwrap();
+        println!("Request took: {} ms", (delta as f64) / 1000000.0);
+        Ok(res)
+    }
+}
+
+fn hello_world(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((iron::StatusCode::OK, "Hello World")))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut chain = Chain::new(hello_world);
+    chain.link_before(ResponseTime);
+    chain.link_after(ResponseTime);
+    Iron::new(chain).http("localhost:3000");
 }
